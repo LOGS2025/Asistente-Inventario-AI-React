4299c22d07e9632cea733c750938876c1c897147
LOGS2025 <225sebas@gmail.com>
2026-04-23 09:28:21 -0600

    ButtonPOST Formulario.jsx added

--- src/app/Component/Form/Formulario.jsx
+++ src/app/Component/Form/Formulario.jsx
@@ -1,47 +1,36 @@
 "use client"
 import { useRef, useEffect, useState } from "react"
 
+  // POST A SUPABASE
+const send_to_supabase = async (nombre,descripcion,precio,categoria)=>{
+  const payload = {
+    "nombre" : nombre,
+    "descripcion" : descripcion,
+    "precio" : precio,
+    "categoria" : categoria,
+  }
+  const response = await fetch('/API/productos/producto_crear', {
+  method: 'POST',
+  headers: {
+  'Content-Type': 'application/json',
+  },
+    body: JSON.stringify(payload)
+  });
+  const result = await response.json();
+  console.log(result);
+};
+  // UPDATE
+
+  // SOFT DELETE
+  
+
 export const Formulario = ()=>{
   const inputRef = useRef();
   const [inputView, setInputView] = useState([]);
 
-  const callOllamaAPI = async (prompt) => {
-    console.log("Calling API...");
-        // In browser console
-    const res = await fetch('/API/IA/prompt_post', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ prompt })
-    })
-    const data = await res.json();
-    if(data.success){
-      return data.message;
-    } else {
-      return 'No se pudo completar la request a ollama.'
-    }
-  };
-
-  const send_to_supabase = async (nombre,descripcion,precio,categoria)=>{
-    const objectPayload_dante = {
-      "nombre" : nombre,
-      "descripcion" : descripcion,
-      "precio" : precio,
-      "categoria" : categoria,
-    }
-    const response = await fetch('/API/productos/producto_crear', {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    },
-      body: JSON.stringify(payload)
-    });
-    const result = await response.json();
-    console.log(result);
-  }
-
   return (
   <>
-    <h1>Hola</h1>
+    <h1>Formulario</h1>
     <div>
       <label>Nombre</label>
       <input 
@@ -50,37 +39,12 @@ export const Formulario = ()=>{
       />
     </div>
 
-    <button 
-      onClick={async ()=>{
-        const result = await callOllamaAPI(generarProductoConIA(inputRef.current.value));
-        console.log(result);
+    <ButtonPOST
+    inputRef={inputRef}
+    inputView={inputView}
+    setInputView={setInputView}
+    />
 
-  // {"nombre": "Dinosaurio", 
-  // "descripcion": "Un modelo a escala de un dinosaurio prehist\u00f3rico con detalles precisos y materiales de alta calidad.", 
-  // "precio": "2499", 
-  // "categoria": "Juguetes y Figuras"}
-
-        if(result){
-          const producto_array = [
-            result.nombre,
-            result.descripcion,
-            result.precio,
-            result.categoria,
-          ];
-          setInputView(producto_array);
-        }
-      }}
-      >Obtener Descipcion</button>
-      <button
-        onClick={
-          ()=>{
-            console.log(inputView)
-          }
-        }
-      >
-        Revisar inputref
-      </button>
-    
     {inputView.map((el, i) => {
       let content;
       
@@ -105,20 +69,70 @@ export const Formulario = ()=>{
 }
 
 
-const generarProductoConIA = (prompt) => {
-  const prompt_for_ollama = `Genera un producto ficticio para una tienda en línea. 
-  Devuelve SOLO un objeto JSON con esta estructura exacta:
-  {
-    "nombre": "nombre del producto",
-    "descripcion": "descripción detallada del producto",
-    "precio": "precio en números (ejemplo: 19.99)",
-    "categoria": "categoría del producto"
-  }
-  No incluyas texto adicional fuera del JSON. 
-  
-  El precio en pesos mexicanos.
 
-  El nombre del producto es : ${prompt}
-  `;
-  return prompt_for_ollama;
+const ButtonPOST = ({inputRef, inputView, setInputView})=>{
+  
+  const callOllamaAPI = async (prompt) => {
+    console.log("Calling API...");
+        // In browser console
+    const res = await fetch('/API/IA/prompt_post', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ prompt })
+    })
+    const data = await res.json();
+    if(data.success){
+      return data.message;
+    } else {
+      return 'No se pudo completar la request a ollama.'
+    }
+  };
+
+  const generarProductoConIA = (prompt) => {
+    const prompt_for_ollama = `Genera un producto ficticio para una tienda en línea. 
+    Devuelve SOLO un objeto JSON con esta estructura exacta:
+    {
+      "nombre": "nombre del producto",
+      "descripcion": "descripción detallada del producto",
+      "precio": "precio en números (ejemplo: 19.99)",
+      "categoria": "categoría del producto"
+    }
+    No incluyas texto adicional fuera del JSON. 
+    
+    El precio en pesos mexicanos.
+  
+    El nombre del producto es : ${prompt}
+    `;
+    return prompt_for_ollama;
+  }
+
+  const updateProduct = async ()=>{
+    const result = await callOllamaAPI(generarProductoConIA(inputRef.current.value));
+    const parsed_json_result = JSON.parse(result);
+    if(result){
+      const producto_array = [
+        parsed_json_result.nombre,
+        parsed_json_result.descripcion,
+        parsed_json_result.precio,
+        parsed_json_result.categoria,
+      ];
+    setInputView(producto_array);
+    }
+  }
+
+  return (
+  <>
+  <button 
+    onClick={updateProduct}
+    >Obtener Descipcion</button>
+    <button
+      onClick={ async ()=>{
+          const res = await send_to_supabase(inputView[0],inputView[1],inputView[2],inputView[3]);
+        }
+      }
+    >
+      Enviar a supabase
+    </button>
+  </>
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import { Formulario } from "./Component/Form/Formulario";
 import { Productos } from "./Component/Productos/Productos";
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 // 1. El usuario ingresa una idea del producto
 // Component/Form -> Recibe y se envia por medio de post
@@ -20,6 +19,8 @@ import axios from "axios";
 export default function Home() {
   const [productos, setProductos] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(()=>{
     const obtenerProductos = async ()=>{
       const response = await fetch('/API/productos/producto_get');
@@ -36,7 +37,7 @@ export default function Home() {
 
   return (
     <>
-      <h1>Formulario</h1>
+      <h1>Home</h1>
       <Formulario/>
 
       <Productos 
